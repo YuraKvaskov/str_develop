395d31e2f201b87c7caf9d8289197c31b214a506
yura kvaskov <yurakvaskov@MacBook-Air-yura.local>
2024-09-28 08:25:46 +0300

    настраиваем cicd

--- StrProject/settings.py
+++ StrProject/settings.py
@@ -11,7 +11,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
