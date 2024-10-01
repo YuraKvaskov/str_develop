7a9a349568871b48c3af0cef9e587cc5d9466529
yura kvaskov <yurakvaskov@MacBook-Air-yura.local>
2024-10-01 20:18:15 +0300

    добавил автодокументацию

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from .views import PartnerListView, PartnerDetailView, EngineListAPIView, EngineDetailAPIView, CityListView, \
     CityDetailView, TagListView
@@ -15,7 +14,5 @@ urlpatterns = [
     path('cities/<int:pk>/', CityDetailView.as_view(), name='city-detail'),
     path('tags/', TagListView.as_view(), name='tag-list'),
     path('catalog/', include('catalog.urls')),  # Добавляем маршруты приложения catalog
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
