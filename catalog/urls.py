4282d43f407d298ea52fdc877940e868adddf18a
yura kvaskov <yurakvaskov@MacBook-Air-yura.local>
2024-10-05 18:44:33 +0300

    меняем сервер

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,15 +12,33 @@ from api.views import (
     # CatalogDetailView
 )
 
-router = DefaultRouter()
-router.register(r'engines', EngineCatViewSet, basename='engine')
-router.register(r'materials', MaterialViewSet, basename='material')
-router.register(r'groups', GroupViewSet, basename='group')
-router.register(r'spare-parts', SparePartViewSet, basename='sparepart')
-router.register(r'repair-kits', RepairKitViewSet, basename='repairkit')
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('engines/', EngineCatViewSet.as_view({'get': 'list'}), name='engine-list'),
+    path('engines/<int:pk>/', EngineCatViewSet.as_view({'get': 'retrieve'}), name='engine-detail'),
+
+    path('materials/', MaterialViewSet.as_view({'get': 'list'}), name='material-list'),
+    path('materials/<int:pk>/', MaterialViewSet.as_view({'get': 'retrieve'}), name='material-detail'),
+
+    path('groups/', GroupViewSet.as_view({'get': 'list'}), name='group-list'),
+    path('groups/<int:pk>/', GroupViewSet.as_view({'get': 'retrieve'}), name='group-detail'),
+
+    path('spare-parts/', SparePartViewSet.as_view({'get': 'list'}), name='sparepart-list'),
+    path('spare-parts/<int:pk>/', SparePartViewSet.as_view({'get': 'retrieve'}), name='sparepart-detail'),
+
+    path('repair-kits/', RepairKitViewSet.as_view({'get': 'list'}), name='repairkit-list'),
+    path('repair-kits/<int:pk>/', RepairKitViewSet.as_view({'get': 'retrieve'}), name='repairkit-detail'),
+
     path('catalog/', CatalogListView.as_view(), name='catalog-list'),
-    # path('catalog/<int:id>/', CatalogDetailView.as_view(), name='catalog-detail'),
 ]
+# router = DefaultRouter()
+# router.register(r'engines', EngineCatViewSet, basename='engine')
+# router.register(r'materials', MaterialViewSet, basename='material')
+# router.register(r'groups', GroupViewSet, basename='group')
+# router.register(r'spare-parts', SparePartViewSet, basename='sparepart')
+# router.register(r'repair-kits', RepairKitViewSet, basename='repairkit')
+#
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('catalog/', CatalogListView.as_view(), name='catalog-list'),
+#     # path('catalog/<int:id>/', CatalogDetailView.as_view(), name='catalog-detail'),
+# ]
