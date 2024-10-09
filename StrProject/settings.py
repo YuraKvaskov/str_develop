d58ba57e7f61f8c115f93f7197cbf14d5deab3f5
yura kvaskov <yurakvaskov@MacBook-Air-yura.local>
2024-10-09 20:22:06 +0300

    фронтенд изменения

--- StrProject/settings.py
+++ StrProject/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
