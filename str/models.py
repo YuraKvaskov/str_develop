63b0727b791939b1f832ce9de9e64bcf6b3f12dc
yura kvaskov <yurakvaskov@MacBook-Air-yura.local>
2024-11-05 16:44:19 +0300

    убрали null

--- str/models.py
+++ str/models.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class Banner(models.Model):
-    image = models.ImageField(upload_to='banners/')
+    image = models.ImageField(upload_to='heads/')
     created_at = models.DateTimeField(auto_now_add=True)
 
 
