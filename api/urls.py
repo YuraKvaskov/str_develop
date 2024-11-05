d17bedd0576a319a955d1360d6d8a7be3bcec235
yura kvaskov <yurakvaskov@MacBook-Air-yura.local>
2024-11-05 16:35:29 +0300

    убрали null

--- api/urls.py
+++ api/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('cities/<int:pk>/', CityDetailView.as_view(), name='city_detail'),
     path('tags/', TagListView.as_view(), name='tag_list'),
     path('catalog/', include('catalog.urls')),  # Добавляем маршруты приложения catalog
-    path('banner/', BannerView.as_view(), name='banner'),
+    path('head/', BannerView.as_view(), name='head'),
 ]
