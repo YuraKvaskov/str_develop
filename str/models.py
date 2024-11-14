0dbb16e975eaa454c7de6a988a3491756f67ebe6
yura kvaskov <yurakvaskov@MacBook-Air-yura.local>
2024-11-14 20:50:02 +0300

    скролл в выборе городов

--- str/models.py
+++ str/models.py
@@ -31,6 +31,9 @@ class City(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
